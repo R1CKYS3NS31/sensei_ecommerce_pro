c8248c94188d7d11bba67b32baef3cf9219a4590
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-04 12:09:13 +0300

    announcement commit

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,15 +1,17 @@
-import styled from '@emotion/styled'
-import React from 'react'
+import styled from "@emotion/styled";
 
 const Container = styled.div`
-    height: 30px;
-    border-right-color: teal;
-    color: white;
-`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
 export const Announcement = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Container>Super Deal! Free Shipping on Orders Over Ksh. 5,000</Container>
+  );
+};
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Announcement } from '../components/Announcement'
 import { NavBar } from '../components/NavBar'
 
 export const Homepage = () => {
     return (
         <div>
+            <Announcement/>
             <NavBar/>
         </div>
     )
