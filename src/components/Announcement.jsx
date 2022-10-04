7d0970355afcabc2b12d14b1411a950cae1b5beb
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-04 11:59:21 +0300

    NavBar commit

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+const Container = styled.div`
+    height: 30px;
+    border-right-color: teal;
+    color: white;
+`
+export const Announcement = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+import { Search, ShoppingCart } from "@mui/icons-material";
+import { Badge } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
 
@@ -9,33 +11,73 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 const Left = styled.div`
   flex: 1;
-`;
-const Center = styled.div`
-  flex: 1;
-`;
-const Right = styled.div`
-  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 const Language = styled.div`
-    font-size: 14px;
-    cursor: pointer;
-`
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+  border-radius: 5px;
+`;
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
 export const NavBar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
-         <Language>EN</Language>
-         <SearchContainer>
-            input
-            icon
-         </SearchContainer>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{color:'gray',fontSize:16}}/>
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>Sensei E-commerce</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge color="primary" badgeContent={4}>
+              <ShoppingCart/>
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
