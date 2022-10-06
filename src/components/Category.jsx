d691043df129c234b48b967abda263a627279b2d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-06 12:20:06 +0300

    Category commit

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,39 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
-const Title = styled.h1``;
-const Button = styled.button``;
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-weight: 600;
+`;
 
 export const Category = ({ item }) => {
   return (
