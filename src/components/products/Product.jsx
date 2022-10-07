42a4d9ecf3e0d7bc91487f8883014a0566898617
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-07 23:16:20 +0300

    products commit

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -5,6 +5,19 @@ import {
 } from "@mui/icons-material";
 import styled from "styled-components";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -15,6 +28,9 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 const Circle = styled.div`
   width: 200px;
@@ -26,18 +42,6 @@ const Circle = styled.div`
 const Image = styled.img`
   height: 75%;
   z-index: 2;
-`;
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
 const Icon = styled.div`
   width: 40px;
