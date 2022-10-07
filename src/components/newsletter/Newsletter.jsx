24cd8923d7cb1a482d0e746f2253c4255e5b4341
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-07 23:42:44 +0300

    news letter commit

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -7,12 +7,32 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
-const Title = styled.h1``;
-const Description = styled.div``;
-const InputContainer = styled.div``;
-const Button = styled.button``;
-const Input = styled.input``;
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
+const Description = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
+const Button = styled.button`
+  flex: 1;
+`;
+const Input = styled.input`
+  border: none;
+  flex: 8;
+`;
 
 export const Newsletter = () => {
   return (
