970136c8727cf54ff366b1fb22fcb03309e028b1
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-09 12:07:43 +0300

    Product commit

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -5,9 +5,54 @@ import { NavBar } from "../../components/navBar/NavBar";
 import { Newsletter } from "../../components/newsletter/Newsletter";
 
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.img``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  margin: 30px 0px;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(prop) => prop.color};
+`;
+const FilterSize = styled.select``;
+const FilterSizeOption = styled.option``;
 
 export const Product = () => {
   return (
@@ -18,6 +63,34 @@ export const Product = () => {
         <ImgContainer>
           <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
         </ImgContainer>
+        <InfoContainer>
+          <Title>DEnim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
+            fuga ut eligendi ea quas explicabo architecto ex doloremque dolores
+            saepe facere esse fugiat. Laborum ipsam dicta natus maiores ex.
+            Ipsam?
+          </Desc>
+          <Price>Ksh. 500</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+        </InfoContainer>
       </Wrapper>
       <Newsletter />
       <Footer />
