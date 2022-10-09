0037a0e3079450dca833e76b341d3e0d204b0b28
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-09 13:09:05 +0300

    Register and Login commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import { Product } from "./pages/product/Product";
+// import { Product } from "./pages/product/Product";
+import { Register } from "./pages/register/Register";
 // import { Homepage } from "./pages/Homepage";
 // import { ProductList } from "./pages/productList/ProductList";
 
 function App() {
   // return <Homepage />;
   // return <ProductList/>
-  return <Product/>
+  // return <Product/>
+  return <Register/>
 }
 
 export default App;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,38 @@
-import styled from "@emotion/styled/types/base"
+import styled from "styled-components";
 
-const Container = styled.div``
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(255, 255, 255, 0.5))
+    url("https://images.pixels.com/photos/6984661/pixels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+    center;
+`;
+const Wrapper = styled.div``;
+const Form = styled.form``;
+const Input = styled.input``;
+const Title = styled.h1``;
+const Agreement = styled.span``;
+const Button = styled.button;
 
 export const Register = () => {
   return (
-    <Container>register</Container>
-  )
-}
+    <Container>ll
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="name" />
+          <Input placeholder="last name" />
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
