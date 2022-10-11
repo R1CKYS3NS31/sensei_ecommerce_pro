636906f3c2ce79e9a69dfd2c4d211992d1b423c1
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-11 10:00:17 +0300

    Cart commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Cart } from "./pages/cart/Cart";
 import { Login } from "./pages/login/Login";
 // import { Product } from "./pages/product/Product";
 import { Register } from "./pages/register/Register";
@@ -10,7 +11,8 @@ function App() {
   // return <ProductList/>
   // return <Product/>
   // return <Register/>
-  return <Login/>
+  // return <Login/>
+  return <Cart/>
 }
 
 export default App;
