32a90ab3e863c80c9e4fa18a0d5cc03c39394006
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-11 10:50:40 +0300

    Cart commit

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -42,11 +42,26 @@ const Info = styled.div`
 const Summary = styled.div`
   flex: 1;
 `;
-const Product = styled.div``;
-const ProductDetail = styled.div``;
-const Image = styled.img``;
-const Details = styled.div``;
-const PriceDetail = styled.div``;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const PriceDetail = styled.div`
+  flex: 1;
+`;
 const ProductName = styled.span``;
 const ProductID = styled.span``;
 const ProductColor = styled.div``;
