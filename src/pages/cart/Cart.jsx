8944f37b456d8e6c3dd965fb365251506e772199
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-14 00:05:57 +0300

    Responsive commit

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -3,10 +3,12 @@ import styled from "styled-components";
 import { Announcement } from "../../components/announcement/Announcement";
 import { Footer } from "../../components/footer/Footer";
 import { NavBar } from "../../components/navBar/NavBar";
+import { mobile } from "../../responsive";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ padding: "10px" })}
 `;
 const Title = styled.div`
   font-weight: 300;
@@ -27,7 +29,8 @@ const TopButton = styled.button`
     props.type === "filled" ? "black" : "transparent"};
   color: ${(props) => props.type === "filled" && "white"};
 `;
-const TopTexts = styled.div``;
+const TopTexts = styled.div`
+${mobile({ display: "none" })}`;
 const TopText = styled.span`
   text-decoration: underline;
   cursor: pointer;
@@ -36,6 +39,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 const Info = styled.div`
   flex: 3;
@@ -44,6 +48,7 @@ const Info = styled.div`
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 const ProductDetail = styled.div`
   flex: 2;
@@ -82,10 +87,12 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
 `;
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
+  ${mobile({ marginBotton: "20px" })}
 `;
 const Hr = styled.hr`
   background-color: #eee;
