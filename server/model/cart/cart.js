a58b7727684c8bd5e3bd373b6490dade2d4405e6
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-14 15:43:52 +0300

    server model commit

---
+++ server/model/cart/cart.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true, unique: true },
+    products: [
+      {
+        productId: { type: String },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("product", cartSchema);
