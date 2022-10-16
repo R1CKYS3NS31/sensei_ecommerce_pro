094fbf045d30c4148cfc9abbd9775a35b7a36074
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-17 00:28:46 +0300

    server login commit

--- server/index.js
+++ server/index.js
@@ -26,8 +26,8 @@ app.get("/", (req, res) => {
     console.error(error);
   }
 });
-app.use("/api/", userRoute);
-app.use("/api/", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 const server = app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   const host = server.address().address;
--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,18 +1,31 @@
 const router = require("express").Router();
 const user = require("../../model/user/user");
+const CryptoJS = require("crypto-js");
 
 // REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new user({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (error) {
     res.status(500).json(error);
+    console.error(error);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    // const user = await
+  } catch (error) {
     console.error(error);
   }
 });
