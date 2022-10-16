c11c72f6c8452024b45fbe82bb5a5a84fa8be715
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-17 02:02:09 +0300

    server jsonwebtoken and update user commit

--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -1,8 +1,35 @@
-const router = require('express').Router()
+const User = require("../../models/user/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+} = require("../../utils/verifyToken");
 
-router.get('/users', (req,res)=>{
-    res.send('user successfull')
-    console.log('user successfull!');
-})
+const router = require("express").Router();
 
-module.exports=router
+router.get("/users", (req, res) => {
+  res.send("user successfull");
+  console.log("user successfull!");
+});
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    var encrypted = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updateUser);
+  } catch (error) {
+    res.status(500).json(error);
+    console.error(error);
+  }
+});
+
+module.exports = router;
---
+++ server/utils/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) {
+        console.error(err);
+        res.status(403).json("token is not valid!");
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("you are not authenticated");
+  }
+};
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        next()
+    }else{
+        res.status(403).json('you are not authorized')
+    }
+  });
+};
+module.exports = { verifyToken ,verifyTokenAndAuthorization};
