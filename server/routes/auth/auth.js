43ebfc9a6ca9d27cc9941c5e70b0ae9d40cf58b8
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-17 01:30:17 +0300

    server jsonwebtoken commit

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const user = require("../../models/user/User");
 const CryptoJS = require("crypto-js");
 const User = require("../../models/user/User");
+const jwt = require("jsonwebtoken");
 
 // REGISTER
 router.post("/register", async (req, res) => {
@@ -31,11 +31,20 @@ router.post("/login", async (req, res) => {
     !user && res.status(401).json("wrong (username) or password!");
     var decrypted = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
     const OriginalPassword = decrypted.toString(CryptoJS.enc.Utf8);
-    OriginalPassword !== req.body.password && res.status(401).json("wrong password");
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("wrong password");
 
-    const {password,...others}  =user._doc
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "1d" }
+    );
+    const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (error) {
     res.status(500).json(error);
     console.error(error);
