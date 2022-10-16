a8f71fa3865b3b69586e66aaa339b3aa5312bcaa
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-17 00:43:52 +0300

    server login commit

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,16 +1,20 @@
 const router = require("express").Router();
 const user = require("../../model/user/user");
 const CryptoJS = require("crypto-js");
+const { findOne } = require("../../model/user/user");
+
 
 // REGISTER
 router.post("/register", async (req, res) => {
+  var encrypted =  CryptoJS.AES.encrypt(
+    req.body.password,
+    process.env.PASS_SEC
+  ).toString();
+
   const newUser = new user({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASS_SEC
-    ).toString(),
+    password: encrypted,
   });
   try {
     const savedUser = await newUser.save();
@@ -24,8 +28,11 @@ router.post("/register", async (req, res) => {
 // LOGIN
 router.post("/login", async (req, res) => {
   try {
-    // const user = await
+    const user = await user.findOne({username:req.body.username})
+    var decrypted = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+
   } catch (error) {
+    res.status(500).json(error)
     console.error(error);
   }
 });
