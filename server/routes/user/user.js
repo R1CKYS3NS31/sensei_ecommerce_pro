e2ee55cb554d591e8d0a390ec3015f2fccbf170d
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-17 13:03:22 +0300

    server delete user commit

--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -2,6 +2,7 @@ const User = require("../../models/user/User");
 const {
   verifyToken,
   verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
 } = require("../../utils/verifyToken");
 
 const router = require("express").Router();
@@ -34,4 +35,26 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+// Delete
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+    console.error(error);
+  }
+});
+
+// get user
+router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+    console.error(error);
+  }
+});
 module.exports = router;
--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -22,4 +22,13 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     }
   });
 };
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("you are not authorized");
+    }
+  });
+};
+module.exports = { verifyToken, verifyTokenAndAuthorization,verifyTokenAndAdmin };
