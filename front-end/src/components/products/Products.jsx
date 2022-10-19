0eb3a92331478e729a7ad26642713846e03ab886
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-19 13:40:47 +0300

    products commit

--- front-end/src/components/products/Products.jsx
+++ front-end/src/components/products/Products.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../../model/dummyData";
 import { Product } from "./Product";
@@ -9,8 +10,32 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-export const Products = ({cat,filters,sort}) => {
-  console.log(cat,filters,sort);
+export const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch(
+          cat
+            ? `http://0.0.0.0:9000/api/products?category=${cat}`
+            : "http://0.0.0.0:9000/api/products"
+        );
+        const productsJson = await res.json()
+        setProducts(productsJson)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat&&setFilteredProducts(
+      products.filter(item=>)
+    )
+  }, [products,cat,filters])
   return (
     <Container>
       {popularProducts.map((item, i) => (
