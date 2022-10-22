2f8a0c448cfa4f84414c61bd5627b354923274d0
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-22 15:24:40 +0300

    user redux login commit

---
+++ front-end/src/redux/apiCalls.js
@@ -0,0 +1,18 @@
+import { loginFailue, loginStart, loginSuccess } from "./userRedux";
+import { BASE_URL } from "../utils/requestMethods";
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await fetch(`${BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    });
+    const userData = await res.json();
+    dispatch(loginSuccess(userData));
+  } catch (error) {
+    dispatch(loginFailue());
+    console.error(error);
+  }
+};
