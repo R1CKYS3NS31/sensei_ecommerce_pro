7a64c9c2cdbe992d214a64ed0e1fc08660d69313
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-10-24 22:49:50 +0300

    admin commit

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div className="">
+         <h1>Login</h1>
+         <input type="text" name="username" placeholder='username' />
+         <input type="password" name="password" placeholder='password' />
+    </div>
+   
+  )
+}
